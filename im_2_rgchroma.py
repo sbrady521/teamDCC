6c8691279c5390d6c06c8bf3ae2c2c7bf168f637
garyyb <garyybai@gmail.com>
2016-11-29 22:35:20 +1100

    added more comments, removed some debug code

--- im_2_rgchroma.py
+++ im_2_rgchroma.py
@@ -2,7 +2,6 @@ import numpy, cv2
 import matplotlib.pyplot as pyplot
 import sys
 import os
-import subprocess
 from rpy2.robjects import r
 # ATTN:
 # Currently uses R to plot the range of g chroma values
@@ -11,6 +10,7 @@ from rpy2.robjects import r
 # in main() - R_plot_g_values call
 # and uncomment the plot_g_values call above it
 # This will plot a shittier graph with matplotlib.pyplot
+# You also need to install rpy2 (pip install rpy2 should be sufficient)
 
 # Converts an image file to rg chroma and writes it
 # also returns a list of g values
@@ -79,6 +79,7 @@ def R_plot_g_values(g_list, filename):
 
     new_file.close()
 
+    # plot graph using R
     filename = filename + '_g_values.png'
     r('g_vals <- read.table("g_list")')
     r('png(filename = "' + filename + '", width=1000, height = 255)')
@@ -93,10 +94,10 @@ def main(args):
     dir_list = os.listdir(args[1])
     dir_list.sort()
 
-    print dir_list
-
+    # Iterate through files in the given dir
     for file in dir_list:
         if file.endswith('_rgchroma.png') or file.endswith('g_values.png'):
+            # skip converted files and graphs
             continue
         filename = args[1] + '/' + file
         g_values = convertToRGChroma(filename)
