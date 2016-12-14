16bf2a8309ec5b49e10e11059e3c77dffc2a2338
Gary Bai <garyybai@gmail.com>
2016-12-14 17:48:47 +1100

    Getting image paths now working

--- c_src/main.cpp
+++ c_src/main.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
 #include "Sample.h"
 
 int main(int argc, char** argv ) {
+    if (argc != 4) {
+        std::cerr << "Usage: ./Chromaticity sampleDir classifyDir outputDir" << std::endl;
+        return 1;
+    }
+
+    char *samplePath = argv[1];
+    char *classifyPath = argv[2];
+    char *outPath = argv[3];
+
+    std::vector<std::string> sampleFiles;
+    std::vector<std::string> classifyFiles;
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (samplePath)) != NULL) {
+        /* print all the files and directories within directory */
+        while ((ent = readdir (dir)) != NULL) {
+            if (strcmp(ent->d_name,".") == 0 || strcmp(ent->d_name,"..") == 0) continue;
+            sampleFiles.push_back(std::string(ent->d_name));
+        }
+        closedir (dir);
+    } else {
+        /* could not open directory */
+        return EXIT_FAILURE;
+    }
+
+    if ((dir = opendir (classifyPath)) != NULL) {
+        /* print all the files and directories within directory */
+        while ((ent = readdir (dir)) != NULL) {
+            if (strcmp(ent->d_name,".") == 0 || strcmp(ent->d_name,"..") == 0) continue;
+            classifyFiles.push_back(std::string(ent->d_name));
+        }
+        closedir (dir);
+    } else {
+        /* could not open directory */
+        return EXIT_FAILURE;
+    }
+
+    int num_files = sampleFiles.size() < classifyFiles.size() ? sampleFiles.size() : classifyFiles.size();
+
+    for (int i = 0; i < num_files; i++) std::cout << sampleFiles[i] << std::endl;
+
+    int samplecnt = 0;
+
+    /*
     Sample test = Sample();
     try {
         test.SampleImage("bottom_camera/botFram0001.jpg");
@@ -15,4 +63,5 @@ int main(int argc, char** argv ) {
     std::cout << path << std::endl;
     std::string out_path = std::string("top_camera/topFram0001_rgchroma.png");
     test.classifyImage(path, out_path);
+    */
 }
