5de8c171c9229e45581f1b9b6a8552eef02d6b2d
Gary Bai <garyybai@gmail.com>
2017-01-28 16:21:13 +1100

    new filterVerticalPeaks algo

--- c_src/YUVSample2.cpp
+++ c_src/YUVSample2.cpp
@@ -76,7 +76,7 @@ void YUVSample2::classifyImage(std::string path, std::string out_path) {
     this->uv_histogram_.filterBins(400);
     std::clock_t c_end = std::clock();
 
-    std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+    std::cout << "CPU time used: "
               << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << " ms\n";
 
     cv::Mat img = cv::imread(path, cv::IMREAD_COLOR);
