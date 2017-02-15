b3dc5655e9c9773caf490bd30908ad6564048d05
sean <seanbrady521@gmail.com>
2017-02-15 17:20:11 +1100

    minor constant values tweaks

--- c_src/GreenChromaClassifier.cpp
+++ c_src/GreenChromaClassifier.cpp
@@ -9,9 +9,11 @@
 #define PROGRESS 2
 #define PLAYING 3
 #define TOTALPIXELS 1228800
-#define SAMPLESIZE 50
+#define SAMPLESIZE 10
 #define TOPWIDTH 1280
 #define TOPHEIGHT 960
+#define U_RANGE 256
+#define V_RANGE 256
 
 void GreenChromaClassifier::sample(GreenChroma& gc, cv::Mat& top, cv::Mat& bottom, int context) {
     if(gc.binsExist() == false && context == PROGRESS){
@@ -21,7 +23,7 @@ void GreenChromaClassifier::sample(GreenChroma& gc, cv::Mat& top, cv::Mat& botto
     if(context == INITIAL){
         int n_rows = top.rows;
         int n_cols = top.cols;
-        for (int y_pos = n_rows*0.5; y_pos < n_rows; y_pos++) {
+        for (int y_pos = n_rows*0.70; y_pos < n_rows; y_pos++) {
             for (int x_pos = 0; x_pos < n_cols; x_pos++) {
                 int y_val = top.at<cv::Vec3b>(y_pos, x_pos)[0];
                 int u_val = top.at<cv::Vec3b>(y_pos, x_pos)[1];
@@ -35,6 +37,15 @@ void GreenChromaClassifier::sample(GreenChroma& gc, cv::Mat& top, cv::Mat& botto
     }else if(context == PROGRESS){
         int randX;
         int randY;
+
+        //Create temporary UV val array for new values
+
+        std::vector<std::vector<bool> > filteredUV;
+        filteredUV = std::vector<std::vector<bool> >(U_RANGE);
+        for (int i = 0; i < U_RANGE; i++) {
+            filteredUV.at(i) = std::vector<bool>(V_RANGE);
+        }
+
         for(int pix_counter = 0 ; pix_counter < SAMPLESIZE ; pix_counter++){
             //Generate random (x, y) coordinates to sample
             randX = rand() % TOPWIDTH;
@@ -49,9 +60,18 @@ void GreenChromaClassifier::sample(GreenChroma& gc, cv::Mat& top, cv::Mat& botto
             //Check if any U V values within 2 are green
             bool validGreen = possiblyGreen(y_val, u_val, v_val, 1, gc);
             if(validGreen){
-                gc.setGreen(u_val, v_val);
+                filteredUV.at(u_val).at(v_val) = true;;
             }
         }
+        //Plug in new values
+        for(int u = 0 ; u < U_RANGE ; u++){
+            for(int v = 0 ; v < V_RANGE ; v++){
+                if(filteredUV.at(u).at(v) == true){
+                    gc.setGreen(u, v);
+                }
+            }
+        }
+
     }else if(context == PLAYING){
         return;
     }
@@ -104,7 +124,7 @@ void GreenChromaClassifier::classify(GreenChroma& gc, cv::Mat& test, cv::Mat& cl
 bool GreenChromaClassifier::possiblyGreen(int y, int u, int v, int within, GreenChroma& gc){
     for(int u_pos = u - within ; u_pos <= u + within ; u_pos++){
         for(int v_pos = v - within ; v_pos <= v + within ; v_pos++){
-            if(u_pos < 0 || u_pos > 255 || v_pos < 0 || v_pos > 255){ //make sure values arent out of range
+            if(u_pos < 0 || u_pos > U_RANGE || v_pos < 0 || v_pos > V_RANGE){ //make sure values arent out of range
                 continue;
             }
             if(gc.isFiltered(u_pos, v_pos)){
