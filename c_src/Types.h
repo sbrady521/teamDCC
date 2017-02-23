e8237b049c20c32d269a9e43efe698300e260f9f
garyyb <garyybai@gmail.com>
2017-02-23 19:35:53 +1100

    inlined isWhite

--- c_src/Types.h
+++ c_src/Types.h
@@ -39,7 +39,17 @@ public:
         return this->filtered_bins_.at(u_val).at(v_val);
     }
 
-    bool isWhite(int y_val, int u_val, int v_val);
+    inline bool isWhite(int y_val, int u_val, int v_val) {
+        // The L^1 function space has a unit circle shaped by a diamond,
+        // which is similar to the uv_plane white diamond
+        // We want all uv-points within a L^p=1 radius of the centre of the uv plane
+
+        if (y_val <= this->y_expv_) return false;
+
+        int lp1_norm = abs(u_val - 128) + abs(v_val - 128);
+
+        return (lp1_norm < this->y_radius_);
+    }
 
     void sanityCheck();
 };
